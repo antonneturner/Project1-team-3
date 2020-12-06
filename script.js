a21c8ad52fe682c71497eded1f65e3cf7b63ab29
Shannon L Johnson-Martin <shannonljohnson-martin@Shannons-MBP.attlocal.net>
2020-12-05 20:56:02 -0500

    Added book search jquery

--- script.js
+++ script.js
@@ -1,41 +1,65 @@
 $(document).ready(function () {
 
-    $.ajax({
-        type: "GET",
-        url: "https://opentdb.com/api.php?amount=3&category=18&difficulty=easy&type=multiple",
-        dataType: "json",
-        success: function (data) {
+$.ajax({
+    type: "GET",
+    url: "https://opentdb.com/api.php?amount=3&category=18&difficulty=easy&type=multiple",
+    dataType: "json",
+    success: function (data) {
+    console.log(data);
+    var searchValue = "Javascript";
+
+        $.ajax({
+            type: "GET",
+            url: "https://opentdb.com/api.php?amount=3&category=18&difficulty=medium&type=multiple",
+            dataType: "json",
+            success: function (data) {
             console.log(data);
             var searchValue = "Javascript";
 
-            $.ajax({
-                type: "GET",
-                url: "https://opentdb.com/api.php?amount=3&category=18&difficulty=medium&type=multiple",
-                dataType: "json",
-                success: function (data) {
+                $.ajax({
+                    type: "GET",
+                    url: "https://opentdb.com/api.php?amount=5&category=18&difficulty=hard&type=multiple",
+                    dataType: "json",
+                    success: function (data) {
                     console.log(data);
                     var searchValue = "Javascript";
 
-                    $.ajax({
-                        type: "GET",
-                        url: "https://opentdb.com/api.php?amount=5&category=18&difficulty=hard&type=multiple",
-                        dataType: "json",
-                        success: function (data) {
+                        $("#newform").submit(function (data) {
                             console.log(data);
-                            var searchValue = "Javascript";
+                            var search = $("#books").val();
+                            if (search == "") {
+                            alert("Title search information required")
+                            } 
+                        
+                            else {
+                            var url = "";
+                            var img = "";
+                            var title = "";
+                            var author = "";
 
-                            $.ajax({
-                                type: "GET",
-                                url: "https://www.googleapis.com/books/v1/volumes?q=" + searchValue,
-                                dataType: "json",
-                                success: function (data) {
-                                    console.log(data);
-                                }
-                           })
-                        }
-                    });
-                }
-            });
-        }
-    });
+                                $.get("https://www.googleapis.com/books/v1/volumes?q=" + search, function (response) {
+
+                                    for (i = 0; i < response.items.length; i++) {
+                                        title = $('<h6 class="center-align black-text">' + response.items[i].volumeInfo.title + '</h6>');
+                                        author = $('<h5 class="center-align black-text"> By: ' + response.items[i].volumeInfo.authors + '</h5>');
+                                        img = $('<img class="aligning card z-depth-5" id="dynamic"><br><a href=' + response.items[i].volumeInfo.infoLink + '><button id="imagebutton" class="button blue aligning">Read More</button></a>');
+
+                                        url = response.items[i].volumeInfo.imageLinks.thumbnail;
+                                        img.attr('src', url);
+                                        title.appendTo('#reults');
+                                        author.appendTo('#results');
+                                        img.appendTo('#results');
+                                    }
+                                });
+                            }
+                                        return false;
+                        });
+                    }
+                })
+            } 
+              
+        
+        })
+    }
+})
 });
